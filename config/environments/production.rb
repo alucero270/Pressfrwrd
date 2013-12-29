485a5edf5d5250997347663ce037fd3109ece472
alex <alex@Deathstar.(none)>
2013-12-29 16:19:15 -0700

    assets precompiled for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,21 @@ LearnRails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # email enabled in production
+config.action_mailer.default_url_options = { :host => ENV["pressfrwrd.heroku.com"] }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+address: "smtp.gmail.com",
+port: 587,
+domain: ENV["pressfrwrd.heroku.com"],
+authentication: "plain",
+enable_starttls_auto: true,
+user_name: ENV["GMAIL_USERNAME"],
+password: ENV["GMAIL_PASSWORD"]
+}
+
 end
