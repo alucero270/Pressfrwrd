cb1aa0f6fbb85c5d9df7298c626599b246d68e0e
Alex <alex@Deathstar.(none)>
2014-01-18 08:38:09 -0700

    add static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LearnRails::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
   resources :contacts, only: [:new, :create]
   resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
