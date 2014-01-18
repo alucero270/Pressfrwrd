d9e55c25fe16197b4800864e8085a9407f1ab55a
Alex <alex@Deathstar.(none)>
2014-01-18 08:17:45 -0700

    Hartl, 3.1

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 ruby '2.0.0'
-gem 'rails', '4.0.1'
+gem 'rails', '4.0.2'
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -15,14 +15,27 @@ gem 'google_drive'
 gem 'high_voltage'
 gem 'simple_form'
 gem 'zurb-foundation'
-group :development do
+
+group :development, :test do
+	gem 'rspec-rails', '2.13.1'
 	gem 'better_errors'
 	gem 'quiet_assets'
 	gem 'rails_layout'
 	gem 'sqlite3'
 end
+
+group :test do
+	gem 'selenium-webdriver', '2.35.1'
+	gem 'capybara', '2.1.0'
+end
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
+
 group :production do
 	gem 'pg'
 	gem 'rails_12factor'
 	gem 'thin'
 end
+
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-LearnRails::Application.config.secret_key_base = 'dd4801eec9c29a00f8fe4dba921cc1bf4f1e1c093d6d1b064b8c24b815ab34be39b8415f848812ac4127c5dc597d1430e957225e9a82599f44e5e5fa03686898'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+LearnRails::Application.config.secret_key_base = secure_token
