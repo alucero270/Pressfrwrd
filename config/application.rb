6f730fe33d9569bf8112d1e02650c5392537923f
Miklos Fazkeas <mfazekas@szemafor.com>
2014-04-08 11:32:09 +0200

    Migrated to pg

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,8 @@ module Pressfrwrd
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = false
+    
+    ActiveModel::SecurePassword.min_cost = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
