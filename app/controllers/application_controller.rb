03334b6550ffca72974184c35d85c25fc2fb85be
Miklos Fazkeas <mfazekas@szemafor.com>
2014-08-26 17:43:28 +0200

    When record is not found redirect to home with an alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   include SessionsHelper
+
+  private
+
+  def record_not_found
+    flash.alert = 'This data does not exists or you do not have rights for it'
+    redirect_to(root_path)
+  end
 end
